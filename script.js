3a291ac33609942c558d2aa30d3156a7ee0ff71d
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-03-02 18:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	getMakeModel(){
+		return `${this.make} ${this.model}`
+}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar extends Car(make, model, topSpeed) {
+	getTopSpeed(){
+		return this.topspeed
+	}
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed())
 
 // Do not change the code below
 window.Car = Car;
